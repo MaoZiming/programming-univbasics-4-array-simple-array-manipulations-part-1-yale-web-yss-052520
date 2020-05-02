e848fb633681334745d071c8483005f3040bd3b7
Ziming Mao <ziming.mao@yale.edu>
2020-05-02 23:09:12 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,31 @@
+def using_push(array, element)
+  array.push(element)
+end
+
+def using_unshift(array, element)
+  array.unshift(element)
+end
+
+def using_pop (array)
+  array.pop
+end
+
+def pop_with_args (array, number = 2)
+  while number >= 1 do
+    array.pop
+    number -= 1
+  end
+end
+
+def using_shift (array)
+  array.shift
+end
+
+def shift_with_args(array, number = 2)
+  while number >= 1 do
+    array.pop
+    number -= 1
+  end
+end
+
+  
